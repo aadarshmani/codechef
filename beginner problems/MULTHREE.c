bcdf0a78a3df9acd31e3169e92cbb80b4eab02a4
Aadarsh <aadarshmanichy2013@gmail.com>
2020-08-26 18:05:13 +0530

    Codechef DSA linear data structure started

--- beginner problems/MULTHREE.c
+++ beginner problems/MULTHREE.c
@@ -6,22 +6,32 @@ int main(){
 	scanf("%d",&t);
 	for(int i=0;i<t;i++){
 		int d0,d1,temp=0;
-		long long int k=0,sum=0;
+		long long int k=0,sum=0,ans;
 		scanf("%lld%d%d",&k,&d0,&d1);
-		sum=d1+d0;
-		if(k<3) {
-			printf("%s", (sum%3==0)?"YES":"NO");
-			break;
-		}
-		else{
-			for(int j=0;j<k-2;j++){
-				temp=((d0+d1)%10);
-				d0=d1;
-				d1=temp;
-				sum+=d1;
-			}
-		}
-		printf("%s", (sum%3==0)?"YES":"NO");
+		temp=d1+d0;
+		ans=temp;
+		sum=(2*temp) % 10 + (4*temp) % 10 + (8*temp) % 10 + (6*temp) % 10;
+		k -= 2;
+
+	if(k > 0) {
+		ans += (temp % 10);
+		k--;
+	}
+
+	ans += (k / 4) * sum;
+
+	k %= 4;
+
+	int p = 2;
+	while(k--) {
+		ans += (p*temp) % 10;
+		p = (p * 2) % 10;
+	}
+
+		if((ans % 3) == 0)
+		printf("YES\n");
+		else
+		printf("NO\n");
 	}
 	return 0;
 }
